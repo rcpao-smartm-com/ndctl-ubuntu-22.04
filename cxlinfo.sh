4d38a762c4e650ac36fe5cbf6e8d47f1159c293b
Roger C. Pao <roger.pao@smartm.com>
2024-06-04 11:25:40 -0700

    cxlinfo.sh

---
+++ cxlinfo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+# script cxlinfo-$(/usr/bin/date +%Y%m%d-%H%M%S).txt
+
+cat /etc/os-release
+uname -a
+uname -r
+cat /proc/cpuinfo
+sudo dmidecode
+lspci
+lspci | grep -i cxl
+sudo lspci -vvv
+sudo lspci -xxxx
+numactl -H
+lsmem
+ls -l /dev/dax*
+daxctl list
+ls -l /dev/cxl/mem*
+sudo cxl list -vvv
+cxl list -HMui
+# echo 4000 | sudo tee /proc/sys/vm/nr_hugepages
+# ./mlc
+
+# exit
